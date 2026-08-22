2745a69ee978023c38ba26c26835205876011cc5
anujabhang <abhang.apm@gmail.com>
2026-08-22 23:43:24 +0530

    Add Aspire, Azure App Configuration and Scalar

--- host/Parkwise.AppHost/AppHost.cs
+++ host/Parkwise.AppHost/AppHost.cs
@@ -1,5 +1,27 @@
+using Microsoft.Extensions.Configuration;
+
 var builder = DistributedApplication.CreateBuilder(args);
 
-builder.AddProject<Projects.Parkwise_WebApp>("parkwise-webapp");
+var connectionString = builder.Configuration.GetConnectionString("Configuration");
+
+IResourceBuilder<IResourceWithConnectionString> config;
+
+// Check if connection string is provided
+if (!string.IsNullOrEmpty(connectionString))
+{
+    // Use the existing connection
+    config = builder.AddConnectionString("Configuration");
+}
+else
+{
+    // Use the emulator if no connection string is provided
+    config = builder.AddAzureAppConfiguration("Configuration")
+                    .RunAsEmulator(static emulator => emulator.WithLifetime(ContainerLifetime.Persistent)
+                    .WithDataVolume());
+}
+
+builder.AddProject<Projects.Parkwise_WebApp>("parkwise-webapp")
+       .WithReference(config)
+       .WaitFor(config);
 
 await builder.Build().RunAsync();
--- src/Parkwise.WebApp/Program.cs
+++ src/Parkwise.WebApp/Program.cs
@@ -1,3 +1,5 @@
+using Scalar.AspNetCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -9,7 +11,20 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.MapScalarApiReference(options =>
+    {
+        options.Title = "Orion180 Token Service";
+        options.Theme = ScalarTheme.Purple;
+        options.ShowSidebar = true;
+        options.DefaultHttpClient = new KeyValuePair<ScalarTarget, ScalarClient>(ScalarTarget.JavaScript, ScalarClient.Axios);
+        options.DarkMode = true;
+        options.ShowDeveloperTools = DeveloperToolsVisibility.Always;
+    });
+    app.MapGet("/", () => Results.Redirect("/scalar"));
+}
+else
+{
+    app.MapGet("/", () => "Parkwise Web App is running...");
 }
 
 app.UseHttpsRedirection();
